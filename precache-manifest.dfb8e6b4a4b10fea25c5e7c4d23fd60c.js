5d9756b8cf98c025970e06ba465bdb4e5925cdbd
Danil Rodionow <danil.rodionow13@gmail.com>
2020-11-17 19:12:08 +0300

    Updates

---
+++ precache-manifest.dfb8e6b4a4b10fea25c5e7c4d23fd60c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0e44374a3772ebf3b59f4c4d23ba8ec",
+    "url": "/spaceX/index.html"
+  },
+  {
+    "revision": "4ccb2ce539d4dbbe6c9c",
+    "url": "/spaceX/static/css/main.e398446f.chunk.css"
+  },
+  {
+    "revision": "2e8b24d600389977d88e",
+    "url": "/spaceX/static/js/2.df3acf44.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/spaceX/static/js/2.df3acf44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ccb2ce539d4dbbe6c9c",
+    "url": "/spaceX/static/js/main.db2cb6e9.chunk.js"
+  },
+  {
+    "revision": "674d2da5a744fff73ed5",
+    "url": "/spaceX/static/js/runtime-main.bf6e654a.js"
+  },
+  {
+    "revision": "d5ab31ce033fc418fd1af87f7b934e1b",
+    "url": "/spaceX/static/media/logo.d5ab31ce.svg"
+  }
+]);
